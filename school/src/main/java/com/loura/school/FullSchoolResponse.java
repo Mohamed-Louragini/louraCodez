f185d7f5ae2d6355f07c9aabea2d19581d16d36f
louragini <medlouragini@gmail.com>
2023-12-18 23:52:04 +0100

    first commit

---
+++ school/src/main/java/com/loura/school/FullSchoolResponse.java
@@ -0,0 +1,17 @@
+package com.loura.school;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FullSchoolResponse {
+
+    private String name;
+    private String email;
+    List<Student> students;
+}
